337604af60a29a80ce937966d8710cca2b900e50
GG <weebcyberpunk@gmail.com>
2022-05-23 13:37:10 -0300

    Removidas notas desnecessárias de C

--- C/bibliotecas.c
+++
@@ -1,9 +0,0 @@
-// Aqui eu listo algumas bibliotecas interessantes de C
-
-#include<stdio.h> // Standart input e standart output, veja mais no arquivo específico
-
-#include<locale.h> // Para setar conjunto de caracteres. Útil para não ter problemas compilando código com acentuação e etc
-// Exemplo:
-setlocale(LC_ALL, "portuguese");
-
-#include<stdlib.h> // Biblioteca padrão, possui várias funções de parse e operações do OS
